9d9c9ea3951703713bbee78f430dede8043513b2
20pricem <20pricem@gmail.com>
2026-05-31 18:07:26 -0500

    fix slow loading?

--- megan-portfolio/vite.config.js
+++ megan-portfolio/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 5,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4,
+      },
+      webp: {
+        quality: 80,
+      },
+    }),
+  ],
   base: '/meganpricephotog/',
 })
